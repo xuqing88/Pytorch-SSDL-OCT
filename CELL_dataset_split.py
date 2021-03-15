fd02d6b8ed6dafbda1757140d6f62d85a9a6440e
Xu Qing <42113623+xuqing88@users.noreply.github.com>
2021-03-15 10:51:56 +0800

    Add files via upload

--- CELL_dataset_split.py
+++ CELL_dataset_split.py
@@ -12,12 +12,10 @@ from shutil import copyfile
 def img_train_test_split(img_source_dir, train_size):
     """
     Randomly splits images over a train and validation folder, while preserving the folder structure
-
     Parameters
     ----------
     img_source_dir : string
         Path to the folder with the images to be split. Can be absolute or relative path
-
     train_size : float
         Proportion of the original images that need to be copied in the subdirectory in the train folder
     """
@@ -31,6 +29,8 @@ def img_train_test_split(img_source_dir, train_size):
         os.makedirs('./dataset/OCT2017/train_semi')
     if not os.path.exists('./dataset/OCT2017/unlabel'):
         os.makedirs('./dataset/OCT2017/unlabel')
+    if not os.path.exists('./dataset/OCT2017/val'):
+        os.makedirs('./dataset/OCT2017/val')
 
 
     # Get the subdirectories in the main image folder
@@ -43,7 +43,8 @@ def img_train_test_split(img_source_dir, train_size):
             break
 
         train_subdir = os.path.join('./dataset/OCT2017/train_semi', subdir)
-        validation_subdir = os.path.join('./dataset/OCT2017/unlabel', subdir)
+        validation_subdir = os.path.join('./dataset/OCT2017/val', subdir)
+        unlabel_subdir = os.path.join('./dataset/OCT2017/unlabel', subdir)
 
         # Create subdirectories in train and validation folders
         if not os.path.exists(train_subdir):
@@ -52,19 +53,25 @@ def img_train_test_split(img_source_dir, train_size):
         if not os.path.exists(validation_subdir):
             os.makedirs(validation_subdir)
 
+        if not os.path.exists(unlabel_subdir):
+            os.makedirs(unlabel_subdir)
+
         train_counter = 0
         validation_counter = 0
+        unlabel_counter =0
 
         # Calculate how many files for train_org, val and test
         file_counts = len(os.listdir(subdir_fullpath))
         # train_count = int(train_size * file_counts)
         train_count = train_size
-        unlabel_count = int(file_counts - train_count)
+        val_count = train_size
+        unlabel_count = int(file_counts - train_count-val_count)
         temp_list = list(os.listdir(subdir_fullpath))
 
         # File Partition
         random.shuffle(temp_list)
         file_train_list = temp_list[0:train_count]
+        file_val_list = temp_list[train_count:train_count*2]
 
         # Randomly assign an image to train or validation folder
         for filename in os.listdir(subdir_fullpath):
@@ -72,13 +79,18 @@ def img_train_test_split(img_source_dir, train_size):
                 copyfile(os.path.join(subdir_fullpath, filename),
                          os.path.join(train_subdir, filename))
                 train_counter += 1
-            else:
+            elif filename in file_val_list:
                 copyfile(os.path.join(subdir_fullpath, filename),
                          os.path.join(validation_subdir, filename))
                 validation_counter += 1
+            else:
+                copyfile(os.path.join(subdir_fullpath, filename),
+                         os.path.join(unlabel_subdir, filename))
+                unlabel_counter += 1
 
         print('Copied ' + str(train_counter) + ' images to ./dataset/OCT2017/train_semi/' + subdir)
-        print('Copied ' + str(validation_counter) + ' images to ./dataset/OCT2017/unlabel/' + subdir)
+        print('Copied ' + str(validation_counter) + ' images to ./dataset/OCT2017/val/' + subdir)
+        print('Copied ' + str(unlabel_counter) + ' images to ./dataset/OCT2017/unlabel/' + subdir)
 
     print("Split End")
 
@@ -94,6 +106,9 @@ if os.path.exists('./dataset/OCT2017/train_semi/'):
 if os.path.exists('./dataset/OCT2017/unlabel/'):
     shutil.rmtree('./dataset/OCT2017/unlabel/')
 
+if os.path.exists('./dataset/OCT2017/val/'):
+    shutil.rmtree('./dataset/OCT2017/val/')
+
 data_dir = './dataset/OCT2017/train'
-train_size = 20  #number of training samples for each class
+train_size = 20  # number of training samples for each class
 img_train_test_split(data_dir, train_size)
